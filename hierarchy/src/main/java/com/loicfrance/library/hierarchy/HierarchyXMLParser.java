cfcab4bb1f5b58201efd601fe7aa91138691753f
loic.france <loic.france@etu.umontpellier.fr>
2018-09-26 17:37:16 +0200

    Add missing copyright notices

--- hierarchy/src/main/java/com/loicfrance/library/hierarchy/HierarchyXMLParser.java
+++ hierarchy/src/main/java/com/loicfrance/library/hierarchy/HierarchyXMLParser.java
@@ -1,8 +1,26 @@
+/*
+ * Copyright 2018 RichardFrance
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.loicfrance.library.hierarchy;
 
 import android.content.Context;
 import android.util.Xml;
-
 
 import com.loicfrance.library.utils.LogD;
 
@@ -47,7 +65,7 @@ public class HierarchyXMLParser<T> {
 
     private Node<T> readNode(XmlPullParser parser, ObjectFiller<T> filler) throws XmlPullParserException, IOException {
         parser.require(XmlPullParser.START_TAG, namespace, "node");
-        Node<T> currentNode = Node.root((T)null, (Node<T>[])null);
+        Node<T> currentNode = Node.root(null, null);
         String tag, value;
         for (int i = 0; i < parser.getAttributeCount(); i++) {
             tag = parser.getAttributeName(i);
